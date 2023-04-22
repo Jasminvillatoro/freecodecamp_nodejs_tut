7fdcf16666f39c8ac8da7a36f8eb3c9ef3a8d0e9
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2023-04-22 10:11:52 -0400

    node js course modules

--- 3-modules.js
+++ 3-modules.js
@@ -1,10 +1,11 @@
-// Modules
-const john = 'john';
-const peter = 'peter';
-const sayHi = (name) => {
-  console.log(`Hello there ${name}`);
-};
-
-sayHi('Susan');
-sayHi(john);
-sayHi(peter);
+// CommonJS, every file is a module (by default). NodeJS uses CommonJS under the hood
+// Modules(file) - Encapsulated code (only share minimum)
+const { john, peter } = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+// console.log(data);
+// When you import a module you invoke it
+require('./7-mind-grenade');
+// sayHi('Susan');
+// sayHi(john);
+// sayHi(peter);
---
+++ 7-mind-grenade.js
@@ -0,0 +1,8 @@
+const num1 = 5;
+const num2 = 10;
+
+function addValues() {
+  console.log(`The sum is : ${num1 + num2}`);
+}
+// if we invoke a function and the function is required else where it will run
+addValues();
